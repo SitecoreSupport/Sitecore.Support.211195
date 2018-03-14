ddfd58911cc3431ea872f0f4306868ecf92473b5
DK\dmko <dmko@sitecore.net>
2018-03-14 11:02:21 +0200

    Original code

---
+++ src/Sitecore.Support.211195/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.211195")]
+[assembly: AssemblyProduct("Sitecore.Support.211195")]
+[assembly: ComVisible(false)]
